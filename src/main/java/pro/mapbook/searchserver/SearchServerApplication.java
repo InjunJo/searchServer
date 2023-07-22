7a5b7cdff82c5880a2a0729373f8200516e0e0c0
Ryan J <107255371+InjunJo@users.noreply.github.com>
2023-07-22 18:05:13 +0900

    intial commit

---
+++ src/main/java/pro/mapbook/searchserver/SearchServerApplication.java
@@ -0,0 +1,13 @@
+package pro.mapbook.searchserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SearchServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SearchServerApplication.class, args);
+    }
+
+}
